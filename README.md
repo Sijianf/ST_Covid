05813e814d3fa06d8df3ac015d9c272365014d9f
Sijianf <42521033+Sijianf@users.noreply.github.com>
2024-06-26 13:25:07 -0400

    Updated under comments

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
 # Spatial Temporal Analysis of COVID Data (ST_Covid_project)
 
-This project focuses on spatial temporal analysis of COVID-19 data. We have developed a series of basic models to illustrate the differences in their assumptions and outcomes. All models operate under the assumption that the high-level mean is the sum of low-level means: 
-$$\mu^H = \sum_{i=1}^{M} \mu_i^L$$
+This project focuses on spatial temporal analysis of COVID-19 data. We have developed a series of basic models to illustrate the differences in their assumptions and outcomes. 
+
+The basic models (*Model 0 and Model 1*) operate under the assumption that the high-level mean at time point $j$ is the sum of total $M$ low-level means: 
+$$\mu_j^H = \sum_{i=1}^{M} \mu_{i,j}^L$$
+
+The other models estimate the high-level mean at time point $j$ through regression of covariates. Below is an example with lagged time effects: 
+$$\mu_j^H = f(Y_j^H_{case}, Y_j^H_{cumulate}, \mu_{j-1}^H)$$
 
 | Model | Features |
 | ------------ | ------------ |
-| **Model 0 (Basic model)** | No cumulative counts as covariates; No cross-level random effects |
-| **Model 1** | No cumulative counts as covariates; Added cross-level random effects |
-| **Model 2** | Added cumulative counts as covariates ${}^*$; Added cross-level random effects |
+| **Model 0** | No regression for high-level; No lagged time effects as covariates; No cross-level random effects |
+| **Model 1** | No regression for high-level; No lagged time effects as covariates; Added cross-level random effects |
+| **Model 2** | Regression for high-level; Added lagged time effects as covariates; Added cross-level random effects |
+| **Model 3** | Regression for high-level; Added lagged time effects as covariates only in high-level ${}^*$; Added cross-level random effects |
 
-${^*}$ Model 2 also incorporates $\mu_{i,j}^L$ as a covariate, namely the mean rate from the previous time point to address the time series effect.
+${^*}$ Model 3 turns out to be a simpler version of Model 2.
 
-Please the codes in [models_raw.R](https://github.com/Sijianf/ST_Covid_project/blob/main/models_raw.R).
+Please see the codes in [models_raw.R](https://github.com/Sijianf/ST_Covid_project/blob/main/models_raw.R).
--- models_raw.R
+++ models_raw.R
@@ -2,19 +2,17 @@
 ####                          Model 0: Basic model                          ####
 #------------------------------------------------------------------------------#
 # - High-level mean = sum of low-level means
-# - no cumulative counts as covariates
-# - no cross-level random effects 
+# - No cumulative counts as covariates
+# - No cross-level random effects
 
 model0<-nimbleCode({
   
   ### High level deaths 
   DHmu[1,1]<-sum(DLmu[1:M,1])
   Dhigh[1,1]~dpois(DHmu[1,1])
-  log(DHmu[1,1])<-dh0+dh1*log(Yhigh[1,1]+0.001)+Dhv[1]
   for(j in 2:Tot){
     DHmu[1,j]<-sum(DLmu[1:M,j])
     Dhigh[1,j]~dpois(DHmu[1,j])
-    log(DHmu[1,j])<-dh0+dh1*log(Yhigh[1,j]+0.001)+Dhv[j]
   }
   
   ### Low level deaths (M = 46 county)
@@ -55,20 +53,18 @@ model0<-nimbleCode({
 #------------------------------------------------------------------------------#
 ####                               Model 1                                  ####
 #------------------------------------------------------------------------------#
-# - High-level mean = sum of low-level means
-# - no cumulative counts as covariates
-# - added cross-level random effects 
+# - High-level mean = sum of low-level means (as above)
+# - No cumulative counts as covariates
+# - Added cross-level random effects 
 
 model1<-nimbleCode({
   
   ### High level deaths 
   DHmu[1,1]<-sum(DLmu[1:M,1])
   Dhigh[1,1]~dpois(DHmu[1,1])
-  log(DHmu[1,1])<-dh0+dh1*log(Yhigh[1,1]+0.001)+Dhv[1]
   for(j in 2:Tot){
     DHmu[1,j]<-sum(DLmu[1:M,j])
     Dhigh[1,j]~dpois(DHmu[1,j])
-    log(DHmu[1,j])<-dh0+dh1*log(Yhigh[1,j]+0.001)+Dhv[j]
   }
   
   ### Low level deaths (M = 46 county)
@@ -110,9 +106,9 @@ model1<-nimbleCode({
 #------------------------------------------------------------------------------#
 ####                               Model 2                                  ####
 #------------------------------------------------------------------------------#
-# - High-level mean = sum of low-level means
-# - added cumulative counts (and mean rate from the previous time) as covariates 
-# - added cross-level random effects 
+# - High-level mean now is a function of high level cases and cumulative cases
+# - Added lagged time effects for both high and low level models: e.g., YChigh[1,j] and DHmu[1,j-1]
+# - Added cross-level random effects 
 
 model2<-nimbleCode({
   
@@ -168,6 +164,71 @@ model2<-nimbleCode({
   
   #### end model 2 
 })
+
+
+
+#------------------------------------------------------------------------------#
+####                               Model 3                                  ####
+#------------------------------------------------------------------------------#
+# - Simpler version of Model 2 
+# - High-level mean now is a function of high level cases and cumulative cases
+# - Removed lagged time effects in low-level models
+# - Added cross-level random effects 
+
+model3<-nimbleCode({
+  
+  ### High level deaths 
+  DHmu[1,1]<-sum(DLmu[1:M,1])
+  Dhigh[1,1]~dpois(DHmu[1,1])
+  log(DHmu[1,1])<-dh0+dh1*log(Yhigh[1,1]+0.001)+Dhv[1]
+  for(j in 2:Tot){
+    DHmu[1,j]<-sum(DLmu[1:M,j])
+    Dhigh[1,j]~dpois(DHmu[1,j])
+    log(DHmu[1,j])<-dh0+dh1*log(Yhigh[1,j]+0.001)+dh2*log(YChigh[1,j]+0.001)+dh3*log(DHmu[1,j-1]+0.001)+Dhv[j]
+  }
+  
+  ### Low level deaths (M = 46 county)
+  for(i in 1:M){
+    Dlow[i,1]~dpois(DLmu[i,1])
+    log(DLmu[i,1])<-dl0+dl1*log(Ylow[i,1]+0.001)+Dlv[i]+Dhv[1]
+    LDev[i,1]<--2*(Dlow[i,1]*log(DLmu[i,1]+0.001)-(DLmu[i,1]+0.001)-lfactorial(Dlow[i,1]))
+    
+    for(j in 2:Tot){
+      Dlow[i,j]~dpois(DLmu[i,j])
+      log(DLmu[i,j])<-dl0+dl1*log(Ylow[i,j]+0.001)+Dlv[i]+Dhv[j]
+      LDev[i,j]<--2*(Dlow[i,j]*log(DLmu[i,j]+0.001)-(DLmu[i,j]+0.001)-lfactorial(Dlow[i,j]))
+    }
+  }
+  
+  Dev<-sum(LDev[1:M,1:Tot])
+  
+  for (j in 1:Tot){
+    Dhv[j]~dnorm(0,tauDhv)
+  }
+  for (i in 1:M){
+    Dlv[i]~dnorm(0,tauDlv)
+  }
+  dh0~dnorm(0,taudh0)
+  dh1~dnorm(0,taudh[1])
+  dh2~dnorm(0,taudh[2])
+  dh3~dnorm(0,taudh[3])
+  dl0~dnorm(0,taudl0)
+  dl1~dnorm(0,taudl1)
+  
+  tauDlv~dgamma(2,0.5)
+  tauDhv~dgamma(2,0.5)
+  taudh0~dgamma(2,0.5)
+  taudl0~dgamma(2,0.5)
+  taudl1~dgamma(2,0.5)
+  
+  for (k in 1:1){
+    taudh[k]~dgamma(2,0.5)
+  }
+  
+  #### end model 3 
+})
+
+
 
 
 
